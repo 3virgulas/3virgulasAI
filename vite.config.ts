5e8a7661c6376b5bb08593939f9d5d472dbac85e
GabrielSemiao <suporte.sitegeral@gmail.com>
2026-01-26 23:06:18 -0300

    PROMETHEUS 1.5

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
             registerType: 'autoUpdate',
             includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
             manifest: {
-                name: '3Vírgulas Prometheus',
-                short_name: 'Prometheus',
+                name: '3Vírgulas AI',
+                short_name: '3Vírgulas',
                 description: 'IA Sem Censura. Superinteligência 405B.',
                 theme_color: '#000000',
                 background_color: '#000000',
